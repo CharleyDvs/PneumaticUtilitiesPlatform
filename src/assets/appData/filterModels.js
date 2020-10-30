2e725e24a16f385e1cc6d30307ab96612f5a3e65
CharleyDvs <ing.carlosjaime@outlook.com>
2020-10-30 17:33:17 -0600

    Added filters data at filter model file

--- src/assets/appData/filterModels.js
+++ src/assets/appData/filterModels.js
@@ -1,312 +1,426 @@
 export const filterModels = {
-    reciprocating: {
-        .12: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+    .12: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF1E",
+            capacity: ".12",
         },
-        .235: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro de línea principal",
+            model: "AFF2C-02",
+            capacity: ".12",
         },
-        .37: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro micrónico",
+            model: "AM150C-02",
+            capacity: ".12",
         },
-        .57: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro coalescente",
+            model: "AMD150C-02",
+            capacity: ".12",
         },
-        .82: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro submicrónico",
+            model: "AME150C-02",
+            capacity: ".12",
         },
-        1.32: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        1.82: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        3.1: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        4.3: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        6.1: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        9.8: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        12.4: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        23: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        30: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        38: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        50: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        65: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF150C-02",
+            capacity: ".12",
         }
-    },
-    screw: {
-        .12: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+    ],
+    .235: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF2E",
+            capacity: ".235",
         },
-        .235: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro de línea principal",
+            model: "AFF2C-02",
+            capacity: ".12",
         },
-        .37: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro micrónico",
+            model: "AM150C-02",
+            capacity: ".12",
         },
-        .57: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro coalescente",
+            model: "AMD150C-02",
+            capacity: ".12",
         },
-        .82: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro submicrónico",
+            model: "AME150C-02",
+            capacity: ".12",
         },
-        1.32: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        1.82: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        3.1: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        4.3: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        6.1: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        9.8: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        12.4: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        23: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        30: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        38: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        50: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
-        },
-        65: {
-            mainLineFilter: "",
-            refrigeratedAirDryer: "",
-            mistSeparator: "",
-            microMistSeparator: "",
-            membraneAirDryer: "",
-            superMistSeparator: "",
-            odorRemovalFilter: ""
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF150C-02",
+            capacity: ".12",
         }
-    }
+    ],
+    .37: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF3E",
+            capacity: ".37",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF2C-02",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM150C-02",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD250C-03",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME250C-03",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF250C-03",
+            capacity: ".12",
+        }
+    ],
+    .57: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF4E",
+            capacity: ".57",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF4C-03",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM250C-03",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD250C-03",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME250C-03",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF250C-03",
+            capacity: ".12",
+        }
+    ],
+    .82: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF6E",
+            capacity: ".82",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF4C-03",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM250C-03",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD350C-04",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME350C-04",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF350C-04",
+            capacity: ".12",
+        }
+    ],
+    1.32: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF8E",
+            capacity: "1.32",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF8C-04",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM350C-04",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD450C-06",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME450C-06",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF450C-06",
+            capacity: ".12",
+        }
+    ],
+    1.82: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF11E",
+            capacity: "1.82",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF11C-06",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM450C-06",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD450C-06",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME450C-06",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF450C-06",
+            capacity: ".12",
+        }
+    ],
+    3.1: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF15E1",
+            capacity: "3.1",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF22C-10",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM550C-10",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD550C-10",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME550C-10",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF550C-10",
+            capacity: ".12",
+        }
+    ],
+    4.3: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF22E",
+            capacity: "4.3",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF37B-14",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM650C-14",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD650C-14",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME650C-14",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF650C-14",
+            capacity: ".12",
+        }
+    ],
+    6.1: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF37E",
+            capacity: "6.1",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF75B-20",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM850-20",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD850-20",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME850-20",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF850-20",
+            capacity: ".12",
+        }
+    ],
+    9.8: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF55E",
+            capacity: "9.8",
+        },
+        {
+            title: "Filtro de línea principal",
+            model: "AFF75B-20",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro micrónico",
+            model: "AM850-20",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD850-20",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro submicrónico",
+            model: "AME850-20",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF850-20",
+            capacity: ".12",
+        }
+    ],
+    12.4: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF75E",
+            capacity: "12.4",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD910-30",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF900-30",
+            capacity: ".12",
+        }
+    ],
+    23: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF120D",
+            capacity: "23",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD910-30",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF900-30",
+            capacity: ".12",
+        }
+    ],
+    30: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF150D",
+            capacity: "30",
+        },
+        {
+            title: "Filtro coalescente",
+            model: "AMD1010-40",
+            capacity: ".12",
+        },
+        {
+            title: "Filtro eliminador de olores",
+            model: "AMF1000-40",
+            capacity: ".12",
+        }
+    ],
+    38: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF190D",
+            capacity: "38",
+        }
+    ],
+    50: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF240D",
+            capacity: "50",
+        }
+    ],
+    65: [
+        {
+            title: "Secador refrigerativo",
+            model: "IDF370B",
+            capacity: "65",
+        }
+    ]
 }
